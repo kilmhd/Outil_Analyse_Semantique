d23738d8367c5727ba3c2c45c6e28c85a0fb6c08
Meïdrick <35963499+kilmhd@users.noreply.github.com>
2020-03-23 10:46:01 +0100

    suppresion fichier pas utilies

--- lib/myWordCloud/wordcloud.py
+++
@@ -1,8 +0,0 @@
-import flask from flask import request, jsonify
-
-app = flask.Flask(__name__)
-app.config["DEBUG"] = True
-
-@app.route('/', methods=['POST'])
-def home():
-    return '''<script>myCloud()</script>'''
